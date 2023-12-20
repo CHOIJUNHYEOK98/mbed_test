75ee27916137bfbaf7ab4066948a1f5e0d4f85bf
jhchoi <c981126@kookmin.ac.kr>
2023-12-20 14:53:47 +0900

    commit all

---
+++ func.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "func.h"
+
+void func(){
+    printf("func test\n");
+}
+
+int add(int a, int b)
+{
+    int result = 0;
+    result = a + b;
+    printf("add : %d + %d = %d\n",a,b,result);
+    return result;
+}
---
+++ test.c
@@ -0,0 +1,8 @@
+#include "func.h"
+
+int main(){
+    int a = 2, b = 3;
+    func();
+    add(a,b);
+    return 0;
+}
